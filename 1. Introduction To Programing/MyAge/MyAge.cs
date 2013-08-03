06c9f008454f30245604774827eb2f475a2e3df7
Vaskata <vaskata.be@gmail.com>
2013-08-03 18:44:46 +0300

    First HW for TA
    First HW for TelerikAcademy

---
+++ 1. Introduction To Programing/MyAge/MyAge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MyAge
+{
+    static void Main()
+    {
+        int age;
+        Console.Write("Write your age: ");
+        int.TryParse(Console.ReadLine(), out age);
+        Console.Write("Your age after 10 years will be: ");
+        int newAge = age + 10;
+        Console.WriteLine(newAge);
+    }
+}
